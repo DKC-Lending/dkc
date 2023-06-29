ec29c2eba43dbee25ebd4f8c5d997809def7c7b3
diwash-karki <diwashkarki7338@gmail.com>
2023-06-29 13:32:05 -0500

    updated invoice month showing incorrect

--- js/invoices.js
+++ js/invoices.js
@@ -488,7 +488,7 @@ function showPdfPreviewer() {
         tdate = `${month}-01-${year}`;
         $("#pdf-table").append('<tr>' +
             '<td>' + tdate + '</td>' +
-            '<td>' + monthNames[today.getMonth()].toString() + ' Interest' + '</td>' +
+            '<td>' + monthNames[today.getMonth()+1].toString() + ' Interest' + '</td>' +
             '<td>' + $("#pbrate").val() + "%" + '</td>' +
             '<td>' + (($("#plfee").val() == "" || $("#plfee").val() == "x" || $("#plfee").val() == "X") ? "X" : "$" + $("#plfee").val()) + '</td>' +
             '<td>' + "$" + eval(parseFloat(($("#plfee").val() == "" || $("#plfee").val() == "x" || $("#plfee").val() == "X") ? "0.00" : $("#plfee").val()) + parseFloat($("#pbmpayment").val())).toLocaleString(undefined, { minimumFractionDigits: 2 }) + '</td>' +
